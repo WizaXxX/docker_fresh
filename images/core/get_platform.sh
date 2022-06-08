efccaaed6b7d23be5098c70033d6cf34505ec946
Ilya <wizax0@gmail.com>
2022-06-08 10:27:20 +0300

    init (#28)
    Co-authored-by: WizaXxX <wizax01@gmail.com>

--- images/core/get_platform.sh
+++
@@ -1,5 +0,0 @@
-cd /out_files
-
- tar -xvf client*.rpm64.tar.gz --exclude "*thin*.rpm" --exclude "*nls*.rpm" --exclude "license-tools" -C /main_dir/distr
- tar -xvf  rpm64_*.tar.gz --exclude "*crs*.rpm" --exclude "*nls*.rpm" -C /main_dir/distr
-
---
+++ images/core/get_platform_from_20_ver.sh
@@ -0,0 +1,6 @@
+cd /out_files
+
+tar -xvf server64*.tar.gz -C /main_dir/distr
+
+mkdir /main_dir/distr/license
+tar -xvf *license_tools*.tar.gz -C /main_dir/distr/license
